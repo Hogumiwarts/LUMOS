5037722dc3f9923298acbbcddd172ea788a82e3f
이상혁 <leedltkdgur@naver.com>
2025-05-03 17:11:09 +0900

    [Android/Design] 기기목록 화면 스크롤바, 새로고침 버튼 추가, 애니메이션 효과 추가

--- Android/wearable/src/main/java/com/hogumiwarts/lumos/presentation/ui/screens/devices/DevicesScreen.kt
+++ Android/wearable/src/main/java/com/hogumiwarts/lumos/presentation/ui/screens/devices/DevicesScreen.kt
@@ -30,12 +30,47 @@ import androidx.wear.compose.material.Text
 import com.hogumiwarts.lumos.R
 import com.hogumiwarts.lumos.presentation.theme.LUMOSTheme
 
+//
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.ui.unit.dp
+import androidx.wear.compose.foundation.lazy.ScalingLazyColumn
+import androidx.wear.compose.foundation.lazy.items
+import androidx.wear.compose.material.Scaffold
+import com.google.android.horologist.annotations.ExperimentalHorologistApi
+import com.google.android.horologist.compose.layout.AppScaffold
+import com.google.android.horologist.compose.layout.ScalingLazyColumn
+import com.google.android.horologist.compose.layout.ScalingLazyColumnDefaults
+import com.google.android.horologist.compose.layout.ScalingLazyColumnDefaults.ItemType
+import com.google.android.horologist.compose.layout.ScreenScaffold
+import com.google.android.horologist.compose.layout.rememberResponsiveColumnState
+
+
+
 // 기기 정보를 담는 데이터 클래스 (기기명, 상태, 아이콘)
 data class DeviceItem(val name: String, val status: Boolean, val icon: ImageVector)
 
+
+
 // 메인 화면 컴포저블
+@OptIn(ExperimentalHorologistApi::class)
 @Composable
 fun DevicesScreen() {
+
+    val listState = rememberResponsiveColumnState(
+        contentPadding = ScalingLazyColumnDefaults.padding(
+            first = ItemType.SingleButton,
+            last = ItemType.Unspecified,
+        ),
+    )
 
     // 추후 ViewModel 또는 실제 데이터 연동으로 교체 예정
     val devices = listOf(
@@ -49,59 +84,62 @@ fun DevicesScreen() {
     // 활성화 여부에 따라 리스트 분리
     val (activatedDevices, deactivatedDevices) = devices.partition { it.status }
 
-    // 배경 이미지와 전체 레이아웃 박스
-    Box(modifier = Modifier.fillMaxSize()) {
-        Image(
-            painter = painterResource(id = R.drawable.device_background),
-            contentDescription = null,
-            modifier = Modifier.fillMaxSize(),
-        )
+    ScreenScaffold(
+        scrollState = listState,
+    ) {
+// 배경 이미지와 전체 레이아웃 박스
+        Box(modifier = Modifier.fillMaxSize()) {
+            Image(
+                painter = painterResource(id = R.drawable.device_background),
+                contentDescription = null,
+                modifier = Modifier.fillMaxSize(),
+            )
 
-        // 스크롤 가능한 리스트
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(horizontal = 12.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            // 상단 타이틀 + 활성화 영역 구분선
-            item {
-                Text(
-                    text = "나의 기기 목록",
-                    fontSize = 16.sp,
-                    fontWeight = FontWeight.ExtraBold,
-                    modifier = Modifier.padding(top = 28.dp)
-                )
-                Spacer(modifier = Modifier.height(16.dp))
-                DividerWithLabel("활성화")
-                Spacer(modifier = Modifier.height(2.dp))
-            }
+            // 스크롤 가능한 리스트
+            ScalingLazyColumn(
+                columnState = listState,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 12.dp),
+            ) {
+                // 상단 타이틀 + 활성화 영역 구분선
 
-            // 활성화된 기기 리스트 렌더링
-            items(activatedDevices) {
-                DeviceCard(it)
-                Spacer(modifier = Modifier.height(8.dp))
-            }
+                item {
+                    Text(
+                        text = "나의 기기 목록",
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.ExtraBold,
 
-            // 비활성화 영역 구분선
-            item {
-                Spacer(modifier = Modifier.height(16.dp))
-                DividerWithLabel("비활성화")
-                Spacer(modifier = Modifier.height(2.dp))
-            }
+                    )
+                }
+                item {
+                    DividerWithLabel("활성화")
+                }
 
-            // 비활성화된 기기 리스트 렌더링
-            items(deactivatedDevices) {
-                DeviceCard(it)
-                Spacer(modifier = Modifier.height(8.dp))
-            }
+                // 활성화된 기기 리스트 렌더링
+                items(activatedDevices) {
+                    DeviceCard(it)
+                }
 
-            // 마지막 여백
-            item {
-                Spacer(modifier = Modifier.height(36.dp))
+                // 비활성화 영역 구분선
+                item {
+                    DividerWithLabel("비활성화")
+                }
+
+                // 비활성화된 기기 리스트 렌더링
+                items(deactivatedDevices) {
+                    DeviceCard(it)
+                }
+
+                // 마지막 여백
+                item {
+
+                    WearIconButton({})
+                }
             }
         }
     }
+
 }
 
 // 외곽선 그라디언트 브러시 정의
@@ -121,7 +159,9 @@ fun DeviceCard(device: DeviceItem) {
     Surface(
         shape = RoundedCornerShape(20.dp),
         color = Color(0x33FFFFFF), // 반투명 흰색 배경
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 6.dp),
         border = BorderStroke(1.dp, gradientBrush) // 그라디언트 테두리
     ) {
         Row(
@@ -141,14 +181,14 @@ fun DeviceCard(device: DeviceItem) {
                 Column {
                     Text(
                         text = device.name,
-                        fontSize = 15.sp,
+                        fontSize = 13.sp,
                         color = Color.White,
                         fontWeight = FontWeight.ExtraBold
                     )
                     Spacer(modifier = Modifier.padding(2.dp))
                     Text(
                         text = if (device.status) "켜짐" else "꺼짐",
-                        fontSize = 12.sp,
+                        fontSize = 11.sp,
                         color = Color.LightGray
                     )
                 }
@@ -171,18 +211,31 @@ fun DeviceCard(device: DeviceItem) {
 fun DividerWithLabel(label: String) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 16.dp, bottom = 12.dp)
     ) {
         Divider(modifier = Modifier.weight(1f), color = Color.Gray)
         Text(
             text = label,
             color = Color(0xFFD5D5D5),
             modifier = Modifier.padding(horizontal = 8.dp),
-            fontSize = 15.sp
+            fontSize = 13.sp
         )
         Divider(modifier = Modifier.weight(1f), color = Color.Gray)
     }
     Spacer(modifier = Modifier.height(12.dp))
+}
+
+@Composable
+fun WearIconButton(onClick: () -> Unit) {
+    Image(
+        painter = painterResource(id = R.drawable.ic_refresh),
+        contentDescription = "My Image Button",
+        modifier = Modifier
+            .size(54.dp)
+            .clickable(onClick = onClick).padding(top = 24.dp)
+    )
 }
 
 // 프리뷰 (Wear OS 장치에서 시스템 UI 포함 화면 미리보기)
