fd538d247649cac43149fe60d3628bedc144b16e
서미지 <itsanisland@naver.com>
2025-05-08 15:19:50 +0900

    [BE/Feat] 전체 gesture_dataset 폴더를 하나의 zip 파일로 압축하여 다운로드할 수 있도록 기능 추가 S12P31D103-64

--- BE/gesture-sensor-service/src/main/java/com/hogumiwarts/lumos/gesturesensor/service/S3UploadService.java
+++ BE/gesture-sensor-service/src/main/java/com/hogumiwarts/lumos/gesturesensor/service/S3UploadService.java
@@ -2,36 +2,29 @@ package com.hogumiwarts.lumos.gesturesensor.service;
 
 import java.io.File;
 import java.io.FileWriter;
-import java.io.IOException;
-import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Map;
-import java.util.zip.ZipOutputStream;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.util.FileSystemUtils;
 
 import com.hogumiwarts.lumos.gesturesensor.dto.SensorDataRequest;
-import com.hogumiwarts.lumos.gesturesensor.dto.SensorDataRequest.SensorValue;
-import com.hogumiwarts.lumos.gesturesensor.util.S3Uploader;
-import com.hogumiwarts.lumos.gesturesensor.util.S3ZipDownloader;
 import com.opencsv.CSVWriter;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class S3Service {
+public class S3UploadService {
 
-	private final S3Uploader s3Uploader;
-	private final S3ZipDownloader s3ZipDownloader;
+	@Value("${cloud.aws.s3.bucket}")
+	private String bucket;
+
+	private final S3Client s3Client;
 
 	public String saveCsvAndUpload(SensorDataRequest request) {
 		try {
@@ -51,7 +44,7 @@ public class S3Service {
 					"gryo_x", "gryo_y", "gryo_z"
 				});
 
-				for (SensorValue d : request.getData()) {
+				for (SensorDataRequest.SensorValue d : request.getData()) {
 					writer.writeNext(new String[]{
 						String.valueOf(d.getTimestamp()),
 						String.valueOf(d.getLiAccX()),
@@ -67,14 +60,25 @@ public class S3Service {
 				}
 			}
 
-			return s3Uploader.upload(tempPath.toFile(), "gesture_dataset/" + request.getGestureId() + "/" + filename);
+			return upload(tempPath.toFile(), "gesture_dataset/" + request.getGestureId() + "/" + filename);
 		} catch (Exception e) {
 			log.error("CSV 업로드 실패", e);
 			throw new RuntimeException("CSV 생성 또는 업로드 실패");
 		}
 	}
 
-	public Path zipAndDownloadFolder(int folder) throws IOException {
-		return s3ZipDownloader.createZipTempFile(folder);
+	public String upload(File file, String key) {
+		PutObjectRequest putRequest = PutObjectRequest.builder()
+			.bucket(bucket)
+			.key(key)
+			.contentType("text/csv")
+			.build();
+
+		s3Client.putObject(putRequest, file.toPath());
+
+		// 업로드된 객체의 S3 URL 반환
+		return s3Client.utilities()
+			.getUrl(b -> b.bucket(bucket).key(key))
+			.toExternalForm(); // https://{bucket}.s3.{region}.amazonaws.com/{key}
 	}
 }
