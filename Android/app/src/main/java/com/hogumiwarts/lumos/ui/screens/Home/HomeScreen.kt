a4ae370f18ef9fd1b0056525a27cdadcb08c97cd
Jang Hong Jun <jhjun99421@gmail.com>
2025-05-09 17:58:58 +0900

    [Android/Feat] 홈화면 UI 구현 임시 S12P31D103-83

--- Android/app/src/main/java/com/hogumiwarts/lumos/ui/screens/Home/HomeScreen.kt
+++ Android/app/src/main/java/com/hogumiwarts/lumos/ui/screens/Home/HomeScreen.kt
@@ -1,24 +1,42 @@
 package com.hogumiwarts.lumos.ui.screens.Home
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.hogumiwarts.lumos.ui.theme.nanum_square_neo
 
 @Composable
 fun HomeScreen() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .statusBarsPadding()
+            .padding(horizontal = 28.dp)
     ) {
+        Spacer(modifier = Modifier.height(16.dp))
+
         Text(
-            text = "루모스 홈",
-            fontSize = 24.sp,
-            fontWeight = FontWeight.Bold
+            text = "오늘 날짜",
+            fontSize = 14.sp,
+            fontFamily = nanum_square_neo,
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+            modifier = Modifier.fillMaxWidth()
         )
     }
 }
