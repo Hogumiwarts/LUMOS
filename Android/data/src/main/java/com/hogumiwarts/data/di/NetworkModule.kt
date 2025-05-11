2deb71686737d33284e95cd1219e826f95606d35
ajang369 <jhjun99421@gmail.com>
2025-05-12 00:55:00 +0900

    [Android/Feat] DI 모듈 설정 (data 모듈) S12P31D103-83

--- Android/data/src/main/java/com/hogumiwarts/data/di/NetworkModule.kt
+++ Android/data/src/main/java/com/hogumiwarts/data/di/NetworkModule.kt
@@ -1,6 +1,7 @@
 package com.hogumiwarts.data.di
 
 import com.hogumiwarts.data.source.remote.AuthApi
+import com.hogumiwarts.data.source.remote.WeatherApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -32,5 +33,28 @@ object NetworkModule {
     @Provides
     @Singleton
     fun provideAuthApi(retrofit: Retrofit): AuthApi = retrofit.create(AuthApi::class.java)
+
+    // 날씨 API 관련 코드 추가
+    @Provides
+    @Singleton
+    @Named("WEATHER_BASE_URL")
+    fun provideWeatherBaseUrl(): String = "https://api.openweathermap.org/data/2.5/"
+
+    @Provides
+    @Singleton
+    @Named("weatherRetrofit")
+    fun provideWeatherRetrofit(
+        okHttpClient: OkHttpClient,
+        @Named("WEATHER_BASE_URL") baseUrl: String
+    ): Retrofit = Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideWeatherApi(@Named("weatherRetrofit") retrofit: Retrofit): WeatherApi =
+        retrofit.create(WeatherApi::class.java)
 
 }
--- Android/data/src/main/java/com/hogumiwarts/data/di/RepositoryModule.kt
+++ Android/data/src/main/java/com/hogumiwarts/data/di/RepositoryModule.kt
@@ -1,7 +1,9 @@
 package com.hogumiwarts.data.di
 
 import com.hogumiwarts.data.repository.AuthRepositoryImpl
+import com.hogumiwarts.data.repository.weather.WeatherRepositoryImpl
 import com.hogumiwarts.domain.repository.AuthRepository
+import com.hogumiwarts.domain.repository.WeatherRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -17,4 +19,10 @@ abstract class RepositoryModule {
     abstract fun bindAuthRepository(
         impl: AuthRepositoryImpl
     ): AuthRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindWeatherRepository(
+        impl: WeatherRepositoryImpl
+    ): WeatherRepository
 }
