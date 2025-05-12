ce5d449d01e6144d7e9039b0824ee28cde659391
이승지 <gaga9353@naver.com>
2025-05-12 16:27:42 +0900

    [Android/Feat] 스위치 제어 UI 화면 구현 S12P31D103-164

---
+++ Android/app/src/main/java/com/hogumiwarts/lumos/ui/screens/devices/SwitchScreen.kt
@@ -0,0 +1,161 @@
+package com.hogumiwarts.lumos.ui.screens.devices
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchColors
+import androidx.compose.material3.SwitchDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.hogumiwarts.lumos.R
+
+data class Device(
+    val tagNumber: Int,
+    val deviceId: Int,
+    val deviceImg: String,
+    val deviceName: String,
+    val manufactureCode: String,
+    val deviceModel: String,
+    val deviceType: String,
+    val activated: Boolean
+)
+
+@Composable
+fun SwitchScreen() {
+
+    var checked by remember { mutableStateOf(true) }
+    // 더미
+    val device = remember {
+        Device(
+            tagNumber = 1,
+            deviceId = 123,
+            manufactureCode = "MiniBig",
+            deviceImg = "https://example.com/test.png",
+            deviceName = "침대 조명 스위치",
+            deviceType = "스위치",
+            deviceModel = "Smart Switch v2",
+            activated = true
+        )
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(40.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+
+        Text(
+            text = device.deviceName,
+            fontWeight = FontWeight.ExtraBold,
+            fontSize = 20.sp
+        )
+
+        Spacer(modifier = Modifier.height(41.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                "스위치",
+                fontWeight = FontWeight.ExtraBold,
+                fontSize = 18.sp
+            )
+            Switch(
+                checked = checked,
+                onCheckedChange = {
+                    checked = it
+                },
+                colors = SwitchDefaults.colors(
+                    checkedThumbColor = Color.White,
+                    checkedTrackColor = Color(0xff3E4784),
+                    uncheckedThumbColor = Color.White,
+                    uncheckedTrackColor = Color(0xFFB0B0B0)
+                )
+            )
+        }
+
+        Spacer(modifier = Modifier.height(17.dp))
+
+        // TODO: DB에서 imageUrl 받으면 바꾸기
+//        AsyncImage(
+//            model = device.deviceImg,
+//            contentDescription = null,
+//            modifier = Modifier.size(180.dp),
+//            contentScale = ContentScale.Fit
+//        )
+
+        Image(
+            painter = painterResource(id = R.drawable.ic_switch),
+            contentDescription = "스위치",
+            modifier = Modifier.size(250.dp)
+        )
+
+        Spacer(modifier = Modifier.height(54.dp))
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.Start
+        ) {
+
+            Text(
+                "기기 정보",
+                fontWeight = FontWeight.ExtraBold,
+                fontSize = 18.sp
+            )
+
+            Spacer(modifier = Modifier.height(13.dp))
+
+            Text(
+                "제조사 | ${device.manufactureCode}",
+                fontSize = 12.sp
+            )
+
+            Text(
+                "모델명 | ${device.deviceModel}",
+                fontSize = 12.sp
+            )
+            Text(
+                "연결방식 | Wi-Fi",
+                fontSize = 12.sp
+            )
+            Text(
+                "기기 타입 | ${device.deviceType}",
+                fontSize = 12.sp
+            )
+
+        }
+
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SwitchPreview() {
+    SwitchScreen()
+
+}
