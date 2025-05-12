f55b9e469f10748a483a942833e9e312ed503e19
서정후 <jeonghu1010@naver.com>
2025-05-12 15:37:44 +0900

    [Android/Feat] SmartThings 연동 임시 커밋

---
+++ Android/app/src/main/java/com/hogumiwarts/lumos/ui/screens/auth/smartthings/SmartThingsLoadingScreen.kt
@@ -0,0 +1,23 @@
+package com.hogumiwarts.lumos.ui.screens.auth.smartthings
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun SmartThingsLoadingScreen(
+
+) {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        // 로딩 애니메이션
+
+
+        // 로딩 중 안내 텍스트
+
+        // 하단 단계별 안내 UI
+    }
+}
---
+++ Android/app/src/main/java/com/hogumiwarts/lumos/ui/screens/auth/smartthings/SmartThingsState.kt
@@ -0,0 +1,5 @@
+package com.hogumiwarts.lumos.ui.screens.auth.smartthings
+
+data class SmartThingsState(
+    val isloadingDone: Boolean
+)
